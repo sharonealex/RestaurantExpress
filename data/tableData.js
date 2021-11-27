ea62b17c04a884815de0008caaa811234458f81c
Sharon <sharonelizalex@gmail.com>
2021-11-27 12:50:11 +1100

    created schema and added data

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+const tableData = [
+    {
+        customerName : "jane smith",
+        customerEmail: "jane@gmail.com",
+        customerPhone: 6788654455,
+        customerId: 78
+       } 
+];
+
+module.exports = tableData
---
+++ data/waitListData.js
@@ -0,0 +1,10 @@
+const waitListData = [
+   {
+    customerName : "tom smith",
+    customerEmail: "tom@gmail.com",
+    customerPhone: 6788654455,
+    customerId: 67
+   } 
+];
+
+module.exports = waitListData;
